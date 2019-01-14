44bff426e04ae4b70bc5f33b8acbd849e5024398
Tony Colston <tonetheman@gmail.com>
2019-01-14 09:34:13 -0600

    comments

--- main.lua
+++ main.lua
@@ -1,24 +1,44 @@
 
+local FONTSIZE=64
+-- used my love2d for ttf font
 local myFont = nil
+
+-- holds os representation of the current date/time
 local t = nil
+
+-- width and height of the window
 local w = nil
 local h = nil
 
 function love.load()
+	-- save off the width and height for use later
 	w = love.graphics.getWidth()
 	h = love.graphics.getHeight()
-	myFont = love.graphics.newFont("alarm_clock.ttf",64)
+
+	-- load the ttf from the disk
+	myFont = love.graphics.newFont("alarm_clock.ttf",FONTSIZE)
+
+	-- tell love to use the font and set the color
 	love.graphics.setFont(myFont)
+	-- this is blue (rgb) 
 	love.graphics.setColor(0,0,255)
 end
 
 function love.update()
+	-- get the date
 	t = os.date("*t")
 end
 
 function love.draw()
+	-- format the string for the screen
+	-- %02d is zero filled 2 places
 	ts = string.format("%02d:%2d:%02d", t.hour,t.min,t.sec)
+
+	-- determine the width and height of the string
+	-- so we can center it
 	local tw = myFont:getWidth(ts)
 	local th = myFont:getHeight(ts)
+
+	-- draw it
 	love.graphics.print(ts,w/2-(tw/2),h/2-(th/2))
 end
